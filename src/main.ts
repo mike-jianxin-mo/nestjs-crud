04c4df3bad6d91e6c22ce87c10419b4b912044e2
mike.mo <mikemo@mikemos-MacBook-Pro.local>
2022-03-22 23:16:06 +1100

    update file styles

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('User Example')
-  .setDescription('The user module')
-  .setVersion('1.0')
-  .addTag('users')
-  .build();
+    .setTitle('User Example')
+    .setDescription('The user module')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
-  
+
   await app.listen(3000);
 }
 bootstrap();
